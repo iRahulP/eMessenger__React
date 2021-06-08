94cd20f35baae2af603ea322828443df730719b0
iRahulP <rahulpatil.m30@gmail.com>
2021-06-08 20:39:41 +0530

    added mesages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  //declaring state with input initialized to empty string
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (e) => {
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
       <h1>Messenger Clone</h1>
-      <input />
-      <button>Send Message</button>
-      {/* input field */}
-      {/* button */}
+
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={sendMessage}>Send Message</button>
+
+      {
+        messages.map(message => {
+          return <p>{message}</p>
+        })
+      }
+
     </div>
   );
 }
