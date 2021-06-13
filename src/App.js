bc1164bf2a029dd79b4ec7f9d0c98a41f06fb765
rahul patil <rahulpatil.m30@gmail.com>
2021-06-13 13:29:39 +0530

    fixed styles

--- src/App.js
+++ src/App.js
@@ -54,10 +54,10 @@ function App() {
       <h2>Welcome {username}</h2>
       <form className="app__form">
         <FormControl className="app__formControl">
-        <Input className="app__input" placeholder="Enter a Message.." value={input} onChange={event => setInput(event.target.value)}/>
-        <IconButton className="app__iconButton" disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}> 
-        <SendIcon />
-        </IconButton>
+          <Input placeholder="Enter a message..." value={input} onChange={(e) => setInput(e.target.value)} />
+          <IconButton disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>
+            <SendIcon />
+          </IconButton>
         </FormControl>
       </form>
 
